817c548b64393e47f0750538b6dc5847b7f56f5c
A.Rahman Mirza <a.rahmanmirza20@gmail.com>
2024-04-09 09:24:01 +0500

    update

---
+++ 1672.Richest_Customer_Wealth.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximumWealth(self, accounts:list[list[int]]) -> int:
+        return max([sum(customer) for customer in accounts])
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.maximumWealth([[1,2,3],[3,2,1]]))
+    print(s.maximumWealth([[1,5],[7,3],[3,5]]))
+    print(s.maximumWealth([[2,8,7],[7,1,3],[1,9,5]]))
