4daf6de4b05fc9b11474aa43247b361a0fa5a2eb
A.Rahman Mirza <a.rahmanmirza20@gmail.com>
2024-04-10 13:13:39 +0500

    update

---
+++ 2073.Time_Needed_to_Buy_Tickets.py
@@ -0,0 +1,14 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets:list[int], k:int) -> int:
+        total_time = 0
+        for i, j in enumerate(tickets): 
+            if i <= k:
+                total_time += min(tickets[i], tickets[k])
+            else:
+                total_time += min(tickets[i], tickets[k]-1)
+        return total_time 
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.timeRequiredToBuy([2,3,2], 2))
+    print(s.timeRequiredToBuy([5,1,1,1], 0))
