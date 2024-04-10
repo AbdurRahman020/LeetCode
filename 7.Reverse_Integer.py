57a35bb7c06eb75f0cf1732b81994c558b31fdd1
A.Rahman Mirza <a.rahmanmirza20@gmail.com>
2024-04-10 21:48:12 +0500

    update

--- 7.Reverse_Integer.py
+++ 7.Reverse_Integer.py
@@ -1,16 +1,27 @@
 class Solution(object):
     def reverse(self, x:int) -> int:
-        num = ''
+        int_reversed = ''
         sign = 1
         for i in str(x):
             if i == '-':
                 sign = -1
             else:
-                num += i
-        num = sign*int(num[::-1])
-        if num > 2**31 - 1 or num < -2**31:
+                int_reversed += i
+        int_reversed = sign*int(int_reversed[::-1])
+        if int_reversed > 2**31 - 1 or int_reversed < -2**31:
             return 0
-        return num
+        return int_reversed
+
+#        method # 2
+#        int_reversed = 0
+#        if x >= 0:
+#           int_reversed = int(str(x)[::-1])
+#        else:
+#           int_reversed = int(str(x)[1:][::-1])*-1
+#        
+#        if int_reversed < -2**31 or int_reversed > 2**31 - 1:
+#           return 0        
+#       return int_reversed
 
 if __name__ == '__main__':
     s = Solution()
