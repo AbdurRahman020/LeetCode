5c559d0ef47f195da1738b6a315efeeaf87db1fe
A.Rahman Mirza <a.rahmanmirza20@gmail.com>
2024-04-10 20:30:14 +0500

    update

---
+++ 950.Reveal_Cards_In_Increasing_Order.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+class Solution:
+    def deckRevealedIncreasing(self, deck:list[int]) -> list[int]:
+        deck_reverse = sorted(deck, reverse=True)
+        new_deck = deque()
+        for card in deck_reverse:
+            if len(new_deck) > 1:
+                new_deck.rotate(1)
+            new_deck.appendleft(card)
+        return list(new_deck)
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.deckRevealedIncreasing([17,13,11,2,3,5,7]))
+    print(s.deckRevealedIncreasing([1,1000]))
