aa1d0bf0f33243867fd59e6a26d8b02840703f22
A.Rahman Mirza <a.rahmanmirza20@gmail.com>
2024-04-11 09:38:35 +0500

    update

---
+++ 1716.Calculate_Money_in_Leetcode_Bank.py
@@ -0,0 +1,16 @@
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        day, weeks, total_balance = 1, 0, 0
+        for days in range(1, n+1):
+            total_balance += (weeks + day)
+            day+=1
+            if days%7==0:
+                weeks += 1
+                day = 1
+        return total_balance
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.totalMoney(4))
+    print(s.totalMoney(10))
+    print(s.totalMoney(20))
