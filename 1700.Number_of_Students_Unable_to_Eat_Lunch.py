303ec6109f38dbd5bd1efe03b205b6f4ea4fdf56
A.Rahman Mirza <a.rahmanmirza20@gmail.com>
2024-04-11 13:20:55 +0500

    update

---
+++ 1700.Number_of_Students_Unable_to_Eat_Lunch.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+class Solution:
+    def countStudents(self, students:list[int], sandwiches:list[int]) -> int:
+        student_Queue = deque(students)
+        sandwich_Stack = []
+        for sandwich in reversed(sandwiches):
+            sandwich_Stack.append(sandwich)
+        served = 0
+        while student_Queue and served < len(student_Queue):
+            if sandwich_Stack[-1] == student_Queue[0]:
+                sandwich_Stack.pop()
+                student_Queue.popleft()
+                served = 0
+            else:
+                student_Queue.append(student_Queue[0])
+                student_Queue.popleft()
+                served += 1
+        return len(student_Queue)
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.countStudents([1,1,0,0], [0,1,0,1]))
+    print(s.countStudents([1,1,1,0,0,1], [1,0,0,0,1,1]))
