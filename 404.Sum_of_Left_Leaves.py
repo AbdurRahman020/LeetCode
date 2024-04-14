e6518ae144a91ff5d4f0023723f7be01f9cd6ec4
A.Rahman Mirza <a.rahmanmirza20@gmail.com>
2024-04-14 19:21:07 +0500

    update

---
+++ 404.Sum_of_Left_Leaves.py
@@ -0,0 +1,24 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def sumOfLeftLeaves(self, root:TreeNode) -> int:
+        def dfs(root:TreeNode, Left:bool) -> int:
+            if not root:
+                return 0
+            if not root.left and not root.right and Left:
+                return root.val
+            return dfs(root.left, True) + dfs(root.right, False)
+        return dfs(root, False)
+
+if __name__ == '__main__':
+    s = Solution()
+    
+    r1 = TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))
+    r2 = TreeNode(1)
+    
+    print(s.sumOfLeftLeaves(r1))
+    print(s.sumOfLeftLeaves(r2))
