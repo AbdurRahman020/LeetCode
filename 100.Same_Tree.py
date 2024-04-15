aec50767a4e5567e096b21ceaab3d70e3b5745d3
A.Rahman Mirza <a.rahmanmirza20@gmail.com>
2024-04-15 09:39:07 +0500

    update

---
+++ 100.Same_Tree.py
@@ -0,0 +1,25 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSameTree(self, p:TreeNode, q:TreeNode) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+if __name__ == '__main__':
+    s = Solution()
+    p1 = TreeNode(1, TreeNode(2), TreeNode(3))
+    q1 = TreeNode(1, TreeNode(2), TreeNode(3))
+    print(s.isSameTree(p1, q1))
+    p2 = TreeNode(1, TreeNode(2))
+    q2 = TreeNode(1, None, TreeNode(2))
+    print(s.isSameTree(p2, q2))
+    p3 = TreeNode(1, TreeNode(2), TreeNode(1))
+    q3 = TreeNode(1, TreeNode(1), TreeNode(2))
+    print(s.isSameTree(p3, q3))
---
+++ 543.Diameter_of_Binary_Tree.py
@@ -0,0 +1,25 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def diameterOfBinaryTree(self, root:TreeNode) -> int:
+        self.diameter_max = 0
+        def dfs(root):
+            if not root:
+                return 0
+            left, right = dfs(root.left), dfs(root.right)
+            self.diameter_max = max(self.diameter_max, left + right)
+            return 1 + max(left, right)
+    
+        dfs(root)
+        return self.diameter_max
+
+if __name__ == '__main__':
+    s = Solution()
+    r1 = TreeNode(1, TreeNode(2))
+    r2 = TreeNode(1, TreeNode(2, TreeNode(4), TreeNode(5)), TreeNode(3))
+    print(s.diameterOfBinaryTree(r1))
+    print(s.diameterOfBinaryTree(r2))
