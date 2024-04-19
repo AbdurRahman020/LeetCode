d32435a8c0bfb8b4600bfccc454b87ac147bdfb3
A.Rahman Mirza <a.rahmanmirza20@gmail.com>
2024-04-19 08:54:43 +0500

    update

--- 79.Word_Search.py
+++ 79.Word_Search.py
@@ -1,28 +1,25 @@
 class Solution(object):
     def exist(self, board:list[list[str]], word:str) -> bool:
-        m, n = len(board), len(board[0])
+        row_length, col_length = len(board), len(board[0])
+        
         def dfs(r, c, current):
             if current == len(word):
                 return True
-            if r < 0 or c < 0 or \
-              r >= m or c >= n or \
-              board[r][c] == '' or \
-              word[current] != board[r][c]:
+            if r < 0 or c < 0 or r >= row_length or c >= col_length or \
+              board[r][c] == '' or word[current] != board[r][c]:
                 return False
             
             cache = board[r][c]
             board[r][c] = ''
             
-            if dfs(r+1, c, current+1) or \
-                dfs(r-1, c, current+1) or \
-                 dfs(r, c+1, current+1) or \
-                  dfs(r, c-1, current+1):
+            if dfs(r+1, c, current+1) or dfs(r-1, c, current+1) or \
+                 dfs(r, c+1, current+1) or dfs(r, c-1, current+1):
                 return True
             
             board[r][c] = cache
             return False
         
-        return any(dfs(i, j, 0) for i in range(m) for j in range(n))
+        return any(dfs(i, j, 0) for i in range(row_length) for j in range(col_length))
 
 if __name__ == '__main__':
     s = Solution()
