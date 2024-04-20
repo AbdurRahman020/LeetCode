a2f7e8e0f71635592ed968f46fa6c70defe493c3
A.Rahman Mirza <a.rahmanmirza20@gmail.com>
2024-04-20 22:06:17 +0500

    update

---
+++ 144.Binary_Tree_Preorder_Traversal.py
@@ -0,0 +1,28 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def preorderTraversal(self, root:TreeNode) -> list[int]:
+        result = []
+        def PreOrder(root):
+            if root != None:
+                result.append(root.val)
+                PreOrder(root.left)
+                PreOrder(root.right)
+        
+        PreOrder(root)
+        return result
+
+if __name__ == '__main__':
+    s = Solution()
+    
+    r1 = TreeNode(1, None, TreeNode(2, TreeNode(3)))
+    r2 = TreeNode(1)
+    r3 = None
+    
+    print(s.preorderTraversal(r1))
+    print(s.preorderTraversal(r2))
+    print(s.preorderTraversal(r3))
---
+++ 145.Binary_Tree_Postorder_Traversal.py
@@ -0,0 +1,28 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def postorderTraversal(self, root:TreeNode) -> list[int]:
+        result = []
+        def PostOrder(root):
+            if root != None:
+                PostOrder(root.left)
+                PostOrder(root.right)
+                result.append(root.val)
+        
+        PostOrder(root)
+        return result
+
+if __name__ == '__main__':
+    s = Solution()
+    
+    r1 = TreeNode(1, None, TreeNode(2, TreeNode(3)))
+    r2 = TreeNode(1)
+    r3 = None
+    
+    print(s.postorderTraversal(r1))
+    print(s.postorderTraversal(r2))
+    print(s.postorderTraversal(r3))
