aaef532cae5027aac1511d88734f80ff3ccfe9a2
A.Rahman Mirza <a.rahmanmirza20@gmail.com>
2024-04-20 23:16:37 +0500

    update

---
+++ 387.First_Unique_Character_in_a_String.py
@@ -0,0 +1,9 @@
+class Solution:
+    def firstUniqChar(self, s:str) -> int:
+        return min([s.index(ch) for ch in set(s) if s.count(ch) == 1] or [-1])
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.firstUniqChar("aabb"))
+    print(s.firstUniqChar("loveleetcode"))
+    print(s.firstUniqChar("leetcode"))
