1ce2a7300d25aa3d1fefede40fc69aa9380564d8
A.Rahman Mirza <a.rahmanmirza20@gmail.com>
2024-04-22 20:29:02 +0500

    update

--- C++/1.Two_Sum.cpp
+++ C++/1.Two_Sum.cpp
@@ -20,20 +20,20 @@ public:
 };
 
 int main() {
-    Solution l;
-    std::vector<int> result1 = l.twoSum({2, 7, 11, 15}, 9);
+    Solution s;
+    std::vector<int> result1 = s.twoSum({2, 7, 11, 15}, 9);
     for (int num : result1) {
         std::cout << num << " ";
     }
     std::cout << std::endl;
 
-    std::vector<int> result2 = l.twoSum({3, 2, 4}, 6);
+    std::vector<int> result2 = s.twoSum({3, 2, 4}, 6);
     for (int num : result2) {
         std::cout << num << " ";
     }
     std::cout << std::endl;
 
-    std::vector<int> result3 = l.twoSum({-1, -2, -3, -4, -5}, -8);
+    std::vector<int> result3 = s.twoSum({-1, -2, -3, -4, -5}, -8);
     for (int num : result3) {
         std::cout << num << " ";
     }
